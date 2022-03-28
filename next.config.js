bd460b8ab963d193fbb0973e3952c34f066c77f5
Ahmed Mohammed <84400676+Ahmed-Mohammed-Badawi@users.noreply.github.com>
2022-03-28 17:07:43 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     reactStrictMode: true,
     env: {
-        DB_LOCAL_URL: "mongodb://localhost:27017/bookit",
+        DB_LOCAL_URL: "mongodb+srv://Sni9er:01120808410@cluster0.vpme4.mongodb.net/BookIt?retryWrites=true&w=majority",
     },
     images: {
         domains: ["a0.muscache.com", "res.cloudinary.com"],
